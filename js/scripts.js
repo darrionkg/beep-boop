7874cee9d83e3b11a63ff211cdc79786060443a4
Darrion <darrionkg@gmail.com>
2019-04-05 11:38:49 -0700

    Add createTable function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Business Logic
 function convertNumber(number) {
   outputArray = [];
   numberArray = [];
@@ -19,26 +20,52 @@ function convertNumber(number) {
 function getTable(number) {
   rows = 0;
   cols = 0;
+  table = [];
   numberSquared = Math.sqrt(number);
   for(var i=1; i <= numberSquared; i++) {
     cols += 1;
     rows += 1;
   }
   if (number % numberSquared !== 0) {
+    cols += 1;
     rows += 1;
   }
-  console.log(rows);
-  console.log(cols);
-}
+  table.push(rows);
+  table.push(cols);
+  return table;
+};
 
+function createTable(table, result) {
+  var htmlsTable = document.getElementById("myTable");
+  var rows = [];
+  var cells = [];
+  var cellCounter = 0;
+  for(var i = 0; i < table[0]; i++) {
+    rows[i] = htmlsTable.insertRow(i);
+    for(var j = 0; j < table[1]; j++) {
+      cells[cellCounter] = rows[i].insertCell(j);
+      cellCounter++;
+    }
+  }
+  console.log(cells.length);
+  for(var k = 0; k < cells.length; k++) {
+    if(result[k] === undefined) {
+      cells[k].innerHTML = " ";
+    }
+    else {
+      cells[k].innerHTML = result[k];
+    }
+  }
+};
 
-//user interface
+//User Interface Logic
 $(document).ready(function() {
   $("#userInput").submit(function(event) {
     event.preventDefault();
     var number = $("input#number").val();
-    var result = convertNumber(number);
     var table = getTable(number);
+    var result = convertNumber(number);
+    var makeTable = createTable(table, result);
     $("#result").show();
     $("#result").text(result);
     console.log(result);
