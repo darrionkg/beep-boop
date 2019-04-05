2459e03f3f1d65385df33ed08f214449f37f72ec
Darrion <darrionkg@gmail.com>
2019-04-05 08:29:37 -0700

    Add initial UI form

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
+function convertNumber(number) {
 
+};
+
+
+//user interface
+$(document).ready(function() {
+  $("#userInput").submit(function(event) {
+    event.preventDefault();
+    var number = $("input#number").val();
+    var result = convertNumber(number);
+    $("#result").show();
+    $("#result").text(result);
+
+
+  });
+});
